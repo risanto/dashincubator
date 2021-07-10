1f18770361bfa39135cf06a0b6aeb7cf7a89f32d
Risan <potallegta@gmail.com>
2021-07-10 21:36:34 +0700

    feat: add seeTaskActivity

--- client/src/api/tasksApi.js
+++ client/src/api/tasksApi.js
@@ -11,8 +11,8 @@ export const getTask = async (id) =>
 export const getTaskActivity = async (id) =>
   dashincubatorRequest(`tasks/get/${id}/activity`, RequestTypes.Get);
 
-export const seeTaskActivity = async (id) =>
-  dashincubatorRequest(`tasks/get/${id}/see-activity`, RequestTypes.Get);
+export const seeTaskActivity = (id) =>
+  dashincubatorRequest(`tasks/get/${id}/see-activity`, RequestTypes.Put);
 
 export const createTask = async (task, bountyID) =>
   dashincubatorRequest("tasks/new", RequestTypes.Post, { task, bountyID });
