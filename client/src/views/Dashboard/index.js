242ffd18d39a8601d46186f23612f19f12bb734d
canis-sirius <david.zhu904@gmail.com>
2021-07-22 17:20:43 -0400

    fix: lint issues

--- client/src/views/Dashboard/index.js
+++ client/src/views/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, {useState, useEffect} from "react";
 import { createUseStyles } from "react-jss";
 import MainLayout from "../../layouts/MainLayout";
 import { fetchNotifications } from "../../api/global";
